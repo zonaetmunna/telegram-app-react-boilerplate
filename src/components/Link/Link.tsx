9a1bee57930deeddcbc6e04d330c8b57b1dd8b36
MD. Zonaet Hossain Munna <zonaetmonna@gmail.com>
2025-07-12 21:48:00 +0600

    fixed

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,39 +1,50 @@
-import { openLink } from '@telegram-apps/sdk-react';
-import { clsx } from 'clsx';
-import { useCallback, type FC, type MouseEventHandler } from 'react';
-import { Link as RouterLink, type LinkProps } from 'react-router-dom';
+import { openLink } from "@telegram-apps/sdk-react";
+import { clsx } from "clsx";
+import { useCallback, type FC, type MouseEventHandler } from "react";
+import { Link as RouterLink, type LinkProps } from "react-router-dom";
 
-import './Link.css';
+import "./Link.css";
 
-export const Link: FC<LinkProps> = ({ className, onClick: propsOnClick, to, ...rest }) => {
-	const onClick = useCallback<MouseEventHandler<HTMLAnchorElement>>(
-		(e) => {
-			propsOnClick?.(e);
+export const Link: FC<LinkProps> = ({
+  className,
+  onClick: propsOnClick,
+  to,
+  ...rest
+}) => {
+  const onClick = useCallback<MouseEventHandler<HTMLAnchorElement>>(
+    (e) => {
+      propsOnClick?.(e);
 
-			// Compute if target path is external. In this case we would like to open
-			// link using TMA method.
-			let path: string;
-			if (typeof to === 'string') {
-				path = to;
-			} else {
-				const { search = '', pathname = '', hash = '' } = to;
-				path = `${pathname}?${search}#${hash}`;
-			}
+      // Compute if target path is external. In this case we would like to open
+      // link using TMA method.
+      let path: string;
+      if (typeof to === "string") {
+        path = to;
+      } else {
+        const { search = "", pathname = "", hash = "" } = to;
+        path = `${pathname}?${search}#${hash}`;
+      }
 
-			const targetUrl = new URL(path, window.location.toString());
-			const currentUrl = new URL(window.location.toString());
-			const isExternal =
-				targetUrl.protocol !== currentUrl.protocol || targetUrl.host !== currentUrl.host;
+      const targetUrl = new URL(path, window.location.toString());
+      const currentUrl = new URL(window.location.toString());
+      const isExternal =
+        targetUrl.protocol !== currentUrl.protocol ||
+        targetUrl.host !== currentUrl.host;
 
-			if (isExternal) {
-				e.preventDefault();
-				openLink(targetUrl.toString());
-			}
-		},
-		[to, propsOnClick]
-	);
+      if (isExternal) {
+        e.preventDefault();
+        openLink(targetUrl.toString());
+      }
+    },
+    [to, propsOnClick]
+  );
 
-	return (
-		<RouterLink {...rest} to={to} onClick={onClick} className={clsx(className, 'link')} />
-	);
+  return (
+    <RouterLink
+      {...rest}
+      to={to}
+      onClick={onClick}
+      className={clsx(className, "link")}
+    />
+  );
 };
